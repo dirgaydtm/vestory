37dd206509bf3e8369494ffe1bc32c339dc7d3aa
Dirgaaa <dirgayuditama6@gmail.com>
2026-08-24 15:09:53 +0700

    feat(theme): add design tokens and Vestory logo

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Green Palette Design Tokens
+  static const greenLight = Color(0xFFEBF2EB);
+  static const greenLightHover = Color(0xFFE0EBE0);
+  static const greenLightActive = Color(0xFFC0D6BF);
+
+  static const greenNormal = Color(0xFF337B32);
+  static const greenNormalHover = Color(0xFF2E6F2D);
+  static const greenNormalActive = Color(0xFF296228);
+
+  static const greenDark = Color(0xFF265C26);
+  static const greenDarkHover = Color(0xFF1F4A1E);
+  static const greenDarkActive = Color(0xFF173716);
+
+  static const greenDarker = Color(0xFF122B12);
+
+  // Neutral Palette Design Tokens
+  static const neutralLight = Color(0xFFFCFCFC);
+  static const neutralLightHover = Color(0xFFFAFBFA);
+  static const neutralLightActive = Color(0xFFF5F6F5);
+
+  static const neutralNormal = Color(0xFFDEE3DE);
+  static const neutralNormalHover = Color(0xFFC8CCC8);
+  static const neutralNormalActive = Color(0xFFB2B6B2);
+
+  static const neutralDark = Color(0xFFA7AAA7);
+  static const neutralDarkHover = Color(0xFF858885);
+  static const neutralDarkActive = Color(0xFF646664);
+
+  static const neutralDarker = Color(0xFF4E4F4E);
+
+  // Red Palette Design Tokens
+  static const redLight = Color(0xFFFCEEEC);
+  static const redLightHover = Color(0xFFFBE6E2);
+  static const redLightActive = Color(0xFFF7CBC3);
+
+  static const redNormal = Color(0xFFE4583F);
+  static const redNormalHover = Color(0xFFCD4F39);
+  static const redNormalActive = Color(0xFFB64632);
+
+  static const redDark = Color(0xFFAB422F);
+  static const redDarkHover = Color(0xFF893526);
+  static const redDarkActive = Color(0xFF67281C);
+
+  static const redDarker = Color(0xFF501F16);
+
+  // Orange Palette Design Tokens
+  static const orangeLight = Color(0xFFFCF6E6);
+  static const orangeLightHover = Color(0xFFFAF2D9);
+  static const orangeLightActive = Color(0xFFF5E3B0);
+
+  static const orangeNormal = Color(0xFFE0A500);
+  static const orangeNormalHover = Color(0xFFCA9500);
+  static const orangeNormalActive = Color(0xFFB38400);
+
+  static const orangeDark = Color(0xFFA87C00);
+  static const orangeDarkHover = Color(0xFF866300);
+  static const orangeDarkActive = Color(0xFF654A00);
+
+  static const orangeDarker = Color(0xFF4E3A00);
+
+  // Base Design Tokens
+  static const baseWhite = Color(0xFFFFFFFF);
+  static const baseBlack = Color(0xFF000000);
+  static const baseBackground = Color(0xFFFCFCFC);
+
+  // Text Design Tokens
+  static const textHeading = Color(0xFF0C2F0C);
+  static const textPrimary = Color(0xFF191B1A);
+  static const textSecondary = Color(0xFF4E4F4E);
+  static const textWhite = Color(0xFFFFFFFF);
+}
